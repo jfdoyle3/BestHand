8a83fa620b935131bd0cdfa7147749afe7693379
Jim Doyle <indellibleink@gmail.com>
2019-08-31 10:08:31 -0400

    made a 2d List for players hand

--- BestHand/Game.cs
+++ BestHand/Game.cs
@@ -7,51 +7,56 @@ namespace BestHand
     {
         //public Hand p1=new Hand();
         //public Hand p2;
-        List<Card> p1 = new List<Card>();
-        List<Card> p2 = new List<Card>();
+        
         private Deck deck = new Deck();
         public List<Card> shuffled;
         public Game()
         {
             this.shuffled = deck.Shuffle();
         }
-        private void Deal()
+        private List<List<Card>> Deal(int players, int hand)
         {
             // need a Jagged List/Array to put/remove any amount of cards on the Table in any position/cell/index.
             // need a Jagged List/Array to have any amount of players from 1 - 52 / Hand amout 1 - 52 cards.
-            int hand = 7;
-            int players = 2;
-            int numCards = hand * players;
+            
+            //List<List<Card>>  outer List Players  /  inner List Hands
+            
+            //List A: Loop A:  Make new List B -> Loop B: Add items to List B :End Loop B -> Add ListB to List A : End Loop A
 
-            // _Output.ConsoleIt("Deck Start",shuffled);
-            for (int c = 0; c < numCards; c++)
+            List<List<Card>> hands = new List<List<Card>>();
+            
+
+            for (int plyr=0; plyr<players; plyr++)
             {
+                List<Card> cDealt = new List<Card>();
 
-                if (c % players == 1)
+                for (int crd=0; crd<hand; crd++)
                 {
-                    p1.Add(shuffled[c]);
-
+                    cDealt.Add(shuffled[crd]);
+                    shuffled.Remove(shuffled[crd]);
                 }
-                else
-                {
-                    p2.Add(shuffled[c]);
-                }
-
+                hands.Add(cDealt);
             }
 
-
-            // _Output.ConsoleIt("Deck after Deal", shuffled);
-            // _Output.ConsoleIt("Hand:  ", p1);
+            return hands;
         }
-                  
-       // public dynamic Play()
-       // {
-          // Deal(7,2);
-          // _Output.ConsoleIt("Deck after Deal", shuffled);
-          //  _Output.ConsoleIt("P1 Hand:  ", p1);
-           // _Output.ConsoleIt("P2 Hand:  ", p2);
 
+        public void Play()
+        {
+            List<List<Card>> hands=Deal(2,7);
 
-       // }
+            
+
+            int cCount = 0;
+            for (int i = 0; i < hands.Count; i++)
+            {
+                for (int j = 0; j < hands[i].Count; j++)
+                {
+                    cCount++;
+                    Console.WriteLine("Player {2} Card {3}:  {0} {1}", hands[i][j].Value, hands[i][j].SuitSym(), i, cCount);
+                }
+            }
+
+        }
     }
 }
