2c291a90650a2f5059a8b2a85ed6e67a6201577a
Jim Doyle <indellibleink@gmail.com>
2019-08-30 20:50:29 -0400

    commented in Card class on refactoring enhancemnts

--- BestHand/Card.cs
+++ BestHand/Card.cs
@@ -8,6 +8,10 @@ namespace BestHand
         public int Suit;
         private string[] Suits = { "S", "H", "C", "D" };
 
+        // Need to add in J Q K A , replacing 11,12,13,14 or 1 (Ace)
+        // Ace can be a Bool method True for High / False for Low :  either 
+        // Need to add Jokers (J1 and J2)
+
         public Card()
         {
             Random newCard = new Random();
