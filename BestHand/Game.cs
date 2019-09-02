dec7e4882edd2927a4e099e5d1172ed48e308928
Jim Doyle <indellibleink@gmail.com>
2019-09-02 06:54:35 -0400

    game calculates highest hand and decalare winner

--- BestHand/Game.cs
+++ BestHand/Game.cs
@@ -12,12 +12,12 @@ namespace BestHand
         {
             this.shuffled = deck.Shuffle();
         }
-        private List<List<Card>> Deal(int players, int hand)
+        public List<List<Card>> Deal(int players, int hand)
         {
-        //    // need a Jagged List/Array to put/remove any amount of cards on the Table in any position/cell/index.
-        //    // need a Jagged List/Array to have any amount of players from 1 - 52 / Hand amout 1 - 52 cards.
+            // need a Jagged List/Array to put/remove any amount of cards on the Table in any position/cell/index.
+            // need a Jagged List/Array to have any amount of players from 1 - 52 / Hand amout 1 - 52 cards.
             
-            //List<List<Card>>  outer List Players  /  inner List Hands
+            //List<List<Card>>  outer List: Players  /  inner List: Hands
             
             //List A: Loop A:  Make new List B -> Loop B: Add items to List B :End Loop B -> Add ListB to List A : End Loop A
             // Need to pass card by Index to Players  ..   All players Index 0 from the dck the 1,2,3 .. etc
@@ -40,18 +40,30 @@ namespace BestHand
         {
             List<List<Card>> hands=Deal(2,7);
 
-            
+           
 
-            int cCount = 0;
-            for (int i = 0; i < hands.Count; i++)
+            int p1Total = 0;
+            int p2Total = 0;
+            Console.Write("Player 1: ");
+            for (int p1=0; p1<hands[0].Count; p1++)
             {
-                for (int j = 0; j < hands[i].Count; j++)
-                {
-                    cCount++;
-                    Console.WriteLine("Player {2} Card {3}:  {0} {1}", hands[i][j].Value, hands[i][j].SuitSym(), i, cCount);
-                }
+                p1Total += hands[0][p1].Value;
+                Console.Write("{0}{1} ",hands[0][p1].Value,hands[0][p1].SuitSym());
             }
-
+            Console.Write("  Total:  {0}",p1Total);
+            Console.Write("\nPlayer 2: ");
+            for (int p2=0; p2<hands[1].Count; p2++)
+            {
+                p2Total += hands[1][p2].Value;
+                Console.Write("{0}{1} ", hands[1][p2].Value, hands[1][p2].SuitSym());
+            }
+            Console.Write("  Total:  {0}\n\n", p2Total);
+            if (p1Total>p2Total)
+                Console.WriteLine("Player 1 Wins");
+            if (p1Total<p2Total)
+                Console.WriteLine("Player 2 Wins");
+            if (p1Total==p2Total)
+                Console.WriteLine("Draw");
         }
     }
 }
